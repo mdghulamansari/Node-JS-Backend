9f164ae2ee76b59823abc545a74124fda35a5708
Md.Ghulam Azad Ansari <mdansari@quaratech.com>
2023-02-22 16:20:11 +0530

    fixed: Validation added

--- Assignment-2/script.js
+++ Assignment-2/script.js
@@ -7,42 +7,66 @@ const closestLeapYear = (year) => {
 
 const checkLeapYear = () => {
   let year = parseInt(prompt("Enter any year"));
-  if (year) {
-    if (year % 4 == 0 && (year % 400 == 0 || year % 100 != 0)) {
-      resultEle.innerHTML = `${year} is a leap year`;
-    } else {
-      let nextLeapYear = closestLeapYear(year);
-      resultEle.innerHTML = `${year} is not a leap year. After ${year}, ${nextLeapYear} is next leap year.`;
-    }
-  } else {
+
+  if (isNaN(year) || year <= 0) {
     resultEle.innerHTML = "Invalid year";
+    return;
+  }
+
+  if (year % 4 == 0 && (year % 400 == 0 || year % 100 != 0)) {
+    resultEle.innerHTML = `${year} is a leap year`;
+  } else {
+    let nextLeapYear = closestLeapYear(year);
+    resultEle.innerHTML = `${year} is not a leap year. After ${year}, ${nextLeapYear} is next leap year.`;
   }
 };
 
 // Problem 2
 const PI = 3.14;
+
+const takeInputAndValidate = (msg) => {
+  let num = Number(prompt(msg));
+  if (isNaN(num)) {
+    throw Error("Please enter number only.");
+  }
+
+  if (num < 0) {
+    throw Error("Please enter +ve number.");
+  }
+  return num;
+};
+
 const calculateArea = () => {
   let shape = prompt("Enter the shape (Circle, Triangle, Square, Rectangle)");
-  switch (shape.toLowerCase()) {
-    case "circle":
-      let radius = Number(prompt("Enter radius"));
-      resultEle.innerText = `Area of circle is ${PI * radius * radius}.`;
-      break;
-    case "triangle":
-      let base = Number(prompt("Enter base"));
-      let height = Number(prompt("Enter height"));
-      resultEle.innerText = `Area of triangle is ${(1 / 2) * base * height}.`;
-      break;
-    case "square":
-      let side = Number(prompt("Enter side"));
-      resultEle.innerText = `Area of square is ${side * side}.`;
-      break;
-    case "rectangle":
-      let length = Number(prompt("Enter length"));
-      let width = Number(prompt("Enter width"));
-      resultEle.innerText = `Area of rectangle is ${length * width}.`;
-      break;
-    default:
-      resultEle.innerText = "Invalid Shape.\nPlease try again.";
+  if (!shape) {
+    resultEle.innerText = "Please enter the shape.";
+    return;
+  }
+
+  try {
+    switch (shape.toLowerCase()) {
+      case "circle":
+        let radius = takeInputAndValidate("Enter radius");
+        resultEle.innerText = `Area of circle is ${PI * radius * radius}.`;
+        break;
+      case "triangle":
+        let base = takeInputAndValidate("Enter base");
+        let height = takeInputAndValidate("Enter height");
+        resultEle.innerText = `Area of triangle is ${(1 / 2) * base * height}.`;
+        break;
+      case "square":
+        let side = takeInputAndValidate("Enter side");
+        resultEle.innerText = `Area of square is ${side * side}.`;
+        break;
+      case "rectangle":
+        let length = takeInputAndValidate("Enter length");
+        let width = takeInputAndValidate("Enter width");
+        resultEle.innerText = `Area of rectangle is ${length * width}.`;
+        break;
+      default:
+        resultEle.innerText = "Invalid Shape.\nPlease try again.";
+    }
+  } catch (error) {
+    resultEle.innerText = error.message;
   }
 };
